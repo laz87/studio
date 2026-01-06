ad3d623871cbe66557751b6870a82bf5645e6899
Lazarus Kariuki <lazaruskariuki87@gmail.com>
2026-01-06 11:08:34 +0000

    no it needs to be a honey comb it the yellow at center

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -7,42 +7,34 @@ const letters = ["A", "B", "C", "D", "E", "F", "G"];
 export default function GamePage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-background text-foreground p-4">
-      <div
-        className="grid grid-cols-3 gap-1"
-        style={{ width: "fit-content" }}
-      >
-        <div className="col-start-2">
-          <Hexagon className="bg-gray-300">
+      <div className="flex flex-col items-center">
+        {/* Top Row */}
+        <div className="flex justify-center" style={{ marginBottom: "-28.87px" }}>
+          <Hexagon className="bg-gray-300 mx-1">
             <span className="text-4xl font-bold">{letters[0]}</span>
           </Hexagon>
-        </div>
-        <div>
-          <Hexagon className="bg-gray-300">
+          <Hexagon className="bg-gray-300 mx-1">
             <span className="text-4xl font-bold">{letters[1]}</span>
           </Hexagon>
         </div>
-        <div className="mt-[-28.87px]">
-          <Hexagon className="bg-yellow-400">
+        {/* Middle Row */}
+        <div className="flex justify-center items-center" style={{ marginBottom: "-28.87px" }}>
+          <Hexagon className="bg-gray-300 mx-1">
             <span className="text-4xl font-bold">{letters[2]}</span>
           </Hexagon>
-        </div>
-        <div className="mt-[-28.87px]">
-          <Hexagon className="bg-gray-300">
+          <Hexagon className="bg-yellow-400 mx-1">
             <span className="text-4xl font-bold">{letters[3]}</span>
           </Hexagon>
-        </div>
-        <div className="col-start-1">
-          <Hexagon className="bg-gray-300">
+          <Hexagon className="bg-gray-300 mx-1">
             <span className="text-4xl font-bold">{letters[4]}</span>
           </Hexagon>
         </div>
-        <div className="mt-[-28.87px]">
-          <Hexagon className="bg-gray-300">
+        {/* Bottom Row */}
+        <div className="flex justify-center">
+          <Hexagon className="bg-gray-300 mx-1">
             <span className="text-4xl font-bold">{letters[5]}</span>
           </Hexagon>
-        </div>
-        <div className="mt-[-28.87px]">
-          <Hexagon className="bg-gray-300">
+          <Hexagon className="bg-gray-300 mx-1">
             <span className="text-4xl font-bold">{letters[6]}</span>
           </Hexagon>
         </div>
